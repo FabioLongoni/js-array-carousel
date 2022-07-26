0abb73dc819f61c5d75e111ffc79da815c7d8d11
FabioLongoni <longoni.fabio90@gmail.com>
2022-07-26 15:39:05 +0200

    add-2-array-add-event-listener

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@ const slides = [
 	'./img/05.jpg',
 ]
 
+const slideElement = [];
+
 const slidesWrapper = document.querySelector('.slides-wrapper');
 slidesWrapper.innerHTML = '';
 console.dir(slidesWrapper);
@@ -14,17 +16,37 @@ console.dir(slidesWrapper);
 //contenuto ul
 for(let i = 0; i < slides.length; i++) {
     const srcImg = slides[i];
-    // console.log(srcImg);
-
+    
     //creare li
     const li = document.createElement('li');
     li.className = 'slide';
     
+    //creare condizione in cui si manifesta la classe active
+    if (i === 0) {
+        li.classList.add('active');
+    }
+
     //creare img
     const img = document.createElement('img');
     img.src = srcImg;
 
     //inserire immagine nell'li
     li.append(img);
-    console.log(li,img);
+
+    //inserire contenuto ul dentro ul
+    slidesWrapper.append(li);
+
+    //creare un secondo array
+    slideElement.push(li);
 }
+
+console.log(slideElement);
+
+//creare elemento da ascoltare -arrow-
+
+const arrowNext = document.querySelector('.arrow-next');
+console.log(arrowNext);
+
+arrowNext.addEventListener('click', function () {
+    console.log('prossima slide')
+})
